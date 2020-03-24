3ac2fcbc311d59fbcba4d5d160c6df358ee7ff28
Guillaume <guillaume.bogard@cpexterne.org>
2020-03-24 18:03:38 +0100

    Updated scala version

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-/scala/scala-2.12.9/bin/scala \
+/scala/scala-2.12.11/bin/scala \
   -J-Xmx100m \
   -Djava.security.policy==./jvm-security.policy \
   -classpath "/app/classpath:/app/classpath/*:/app/dependencies/*" \
